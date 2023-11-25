a228af7ba7f62235bfd23c619e70141ddf9dc7b0
DerTaktischeHase <timo.hartstang@mail.de>
2023-11-25 15:47:41 +0100

    just for testing

--- MergeSort7.java
+++ MergeSort7.java
@@ -3,7 +3,7 @@ import java.util.Random;
 public class MergeSort7 {
     public static void main (String[] args) {
         int[] array = { 4, 2, 7, 5 };
-        split(array);
+        split(array);//5
         System.out.println(arrayListToString(array));
     }
 
