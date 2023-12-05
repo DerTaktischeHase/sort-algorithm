6a138914a556c8b965061d3d4a4738625bca493b
DerTaktischeHase <66119685+DerTaktischeHase@users.noreply.github.com>
2023-12-05 21:40:50 +0100

    Add files via upload

--- MergeSort8.java
+++ MergeSort8.java
@@ -1,60 +1,65 @@
+import java.util.Arrays;
+
 public class MergeSort8 {
-  public static void sort (int[] list) {
-    split(list);
-  }
-
-  public static void split (int[] list) {
-    if (list.length == 1) return list;
-
-    // Teilen und Beherrschen
-    // #1 Teilen
-
-    int[] left = new int[list.length/2];
-    int[] right = new int[list.length - (list.length/2)];
-
-    for (int i = 0; i < list.length/2; i++) {
-      left[i] = list[i];
+    public static void sort (int[] list) {
+        split(list);
     }
 
-    for (int i = (list.length/2); i < list.length; i++) {
-      right[i - (list.length/2)] = list[i];
+    public static void split (int[] list) {
+        if (list.length == 1) return;
+
+        // Teilen und Beherrschen
+        // #1 Teilen
+
+        int[] left = new int[list.length/2];
+        int[] right = new int[list.length - (list.length/2)];
+
+        for (int i = 0; i < list.length/2; i++) {
+            left[i] = list[i];
+        }
+
+        for (int i = (list.length/2); i < list.length; i++) {
+            right[i - (list.length/2)] = list[i];
+        }
+
+        split(left);
+        split(right);
+
+        merge(list, left, right);
     }
 
-    split(left);
-    split(right);
+    public static void merge (int[] list, int[] left, int[] right) {
+        int regularIndex = 0, leftIndex = 0, rightIndex = 0;
 
-    merge(list, left, right);
-  }
+        while (leftIndex < left.length && rightIndex+1 < right.length) {
+            if (left[leftIndex] >= right[rightIndex]) {
+                list[regularIndex] = right[rightIndex];
+                regularIndex++;
+                rightIndex++;
+            } else if (right[rightIndex] >= left[leftIndex]){
+                list[regularIndex] = left[leftIndex];
+                regularIndex++;
+                leftIndex++;
+            }
+        }
 
-  public static void merge (int[] list, int[] left, int[] right) {
-    int regularIndex = 0, leftIndex = 0, rightIndex = 0;
+        while (leftIndex < left.length) {
+            list[regularIndex] = left[leftIndex];
+            regularIndex++;
+            leftIndex++;
+        }
 
-    while (leftIndex < left.length && rightIndex < right.length) {
-      if (left[leftIndex] > right[rightIndex]) {
-        list[regularIndex] = right[rightIndex];
-        regularIndex++, rightIndex++;
-      } else if (right[rightIndex] > left[leftIndex]){
-        list[regularIndex] = left[leftIndex];
-        regularIndex++, leftIndex++;
-      }
+        while (rightIndex < right.length) {
+            list[regularIndex] = right[rightIndex];
+            regularIndex++;
+            rightIndex++;
+        }
+
     }
-
-    while (leftIndex < left.length) {
-      list[regularIndex] = left[leftIndex];
-      regularIndex;
-      leftIndex++; 
+    public static void main (String[] args) {
+        int[] list = new int[]{ 4, 6, 2, 4, 8};
+        System.out.println(Arrays.toString(list));
+        split(list);
+        System.out.println(Arrays.toString(list));
     }
-
-    while (rightIndex < right.length) {
-      list[regularIndex] = right[rightIndex];
-      regularIndex++;
-      rightIndex++; 
-    }
-
-  }
-  public static void main (String[] args) {
-    int[] list = new int[]{ 4, 6, 2, 4, 8};
-    sort(list);
-    System.out.println();
-  }
 }
