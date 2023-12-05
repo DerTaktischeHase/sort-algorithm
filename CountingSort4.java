55ff6b252f31bdf9469ddda37a0732f8e1399e18
DerTaktischeHase <66119685+DerTaktischeHase@users.noreply.github.com>
2023-12-05 20:33:34 +0100

    Update CountingSort4.java

--- CountingSort4.java
+++ CountingSort4.java
@@ -1,5 +1,34 @@
 public class CountingSort4 {
+  public static void sort (int[] list) {
+    split(list);
+  }
 
+  public static void split (int[] list) {
+    if (list.length == 1) return list;
+
+    // Teilen und Beherrschen
+    // #1 Teilen
+
+    int[] left = new int[list.length/2];
+    int[] right = new int[list.length - (list.length/2)];
+
+    for (int i = 0; i < list.length/2; i++) {
+      left[i] = list[i];
+    }
+
+    for (int i = (list.length/2); i < list.length; i++) {
+      right[i - (list.length/2)] = list[i];
+    }
+
+    split(left);
+    split(right);
+
+    merge();
+  }
+
+  public static void merge (int[] list, int[] left, int[] right) {
+
+  }
   public static void main (String[] args) {
     
   }
